1888ecf1de3de6dba22f5332b61cb0084d24b9a0
jly <453825818@qq.com>
2019-10-24 20:53:39 +0800

    10-24

---
+++ web/06-css进阶/js/009.js
@@ -0,0 +1,12 @@
+;(function(win,doc){
+	var oRoot = doc.getElementsByTagName('html')[0]
+	console.log(oRoot)
+	function refresh(){
+		var width=doc.body.clientWidth || doc.documentElement.clientWidth;
+		var oFontSize = width/10;
+		oRoot.style.fontSize = oFontSize + 'px';
+	}
+	win.addEventListener('DOMContentLoaded',refresh,false)
+	win.addEventListener('load',refresh,false)
+	win.addEventListener('resize',refresh,false)
+})(window,document);
