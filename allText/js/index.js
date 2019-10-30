aaf46b2b21e1fba5de7cb2571cb07f4947c56cbf
tanglimin <552116918@qq.com>
2019-10-30 23:44:07 +0800

    10-30

--- allText/js/index.js
+++ allText/js/index.js
@@ -1,32 +1,76 @@
 ;(function($){
-	$('.content .father').on('click',function(){
+	$('.father').on('click',function(){
 		$('.content .son1-ul').slideDown();
-		this.style.backgroundColor = 'red';
+		this.style.backgroundColor = 'gray'
 	})
 
 /*大纲分类*/
-	$('.JQuery').on('click',function(){
-		$('.text-son2').slideDown();
-		this.style.backgroundColor = 'red';
-	})
+	const $jq = $('.JQuery')
+	const $ht = $('.HTML')
+	const $css = $('.CSS')
+	const $git = $('.GIT')
+	const $js = $('.JS')
+	const $bs = $('.BootStrap')
+	const $es = $('.ES678')
+	const $wl = $('.WLine')
+	const $no = $('.NodeJs')
+	const $db = $('.MongoDB')
+
+
+	function son1($elem,name){
+		$elem.on('click',function(){
+			$('.son1-btn').css('background','#ccc')
+			$('.text-son2').css('borderTop','1px solid #ccc')
+			$('.text-son2').hide();
+			$('.text-son2-'+name+'').slideDown();
+			$elem.css('background','gray')
+		})
+	}
+
+
+	son1($jq,'jquery')
+	son1($ht,'html')
+	son1($css,'css')
+	son1($git,'git')
+	son1($js,'js')
+	son1($bs,'bootstrap')
+	son1($es,'es678')
+	son1($wl,'wline')
+	son1($no,'nodejs')
+	son1($db,'mongodb')
+
 
 /*要点分类*/
-	$('.text-son2 .no1').on('click',function(){
-		$('.text-son3').slideDown();
-		this.style.backgroundColor = 'red';
+	$('.son2-jquery .no1').on('click',function(){
+		$('.text-son2-jquery .son2-btn').removeClass('active')
+		$('.text-son3').hide();
+		$('.text-son3-1-jquery').slideDown();
+		$('.son2-jquery .no1').addClass('active');
+	})
+	$('.son2-jquery .no2').on('click',function(){
+		$('.text-son2-jquery .son2-btn').removeClass('active')
+		$('.text-son3').hide();
+		$('.text-son3-2-jquery').slideDown();
+		$('.son2-jquery .no2').addClass('active');
 	})
 
 /*详细分类*/
-	$('.text-son3 .no1').on('click',function(){
-		$('.son3-1').slideDown();
-		this.style.backgroundColor = 'red';
+	$('.text-son3-1-jquery .no1').on('click',function(){
+		$('.text-son4').css('borderTop','1px solid #ccc');
+		$('.son4').hide();
+		$('.son4-1').slideDown();
+		this.style.backgroundColor = 'gray';
 	})
-	$('.text-son3 .no2').on('click',function(){
-		$('.son3-2').slideDown();
-		this.style.backgroundColor = 'blue';
+	$('.text-son3-1-jquery .no2').on('click',function(){
+		$('.text-son4').css('borderTop','1px solid #ccc');
+		$('.son4').hide();
+		$('.son4-2').slideDown();
+		this.style.backgroundColor = 'gray';
 	})
-	$('.text-son3 .no3').on('click',function(){
-		$('.son3-3').slideDown();
-		this.style.backgroundColor = 'green';
+	$('.text-son3-1-jquery .no3').on('click',function(){
+		$('.text-son4').css('borderTop','1px solid #ccc');
+		$('.son4').hide();
+		$('.son4-3').slideDown();
+		this.style.backgroundColor = 'gray';
 	})
 })(jQuery);
