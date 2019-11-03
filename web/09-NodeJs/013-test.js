6288cdacec07dbf048b41a04a242d57d620235fb
jly <453825818@qq.com>
2019-11-03 18:00:21 +0800

    11-3

--- web/09-NodeJs/013-test.js
+++ web/09-NodeJs/013-test.js
@@ -1,21 +1,19 @@
 ;(function($){
 	var $input = $('.todo-in')
 	$input.on('keydown',function(ev){
-
-
-		$.ajax({
-			url:url,
-			type:'POST',
-			dataType:'json',
-			success:function(data){
-				console.log(data)
-			},
-			error:function(jqXHR,statusMsg){
-				console.log(statusMsg)
-			}
-		})
 		if(ev.keyCode == 13){
-			
+			$.ajax({
+				url:url,
+				type:'POST',
+				dataType:'json',
+				data:$input.val(),
+				success:function(data){
+					console.log(data)
+				},
+				error:function(jqXHR,statusMsg){
+					console.log(statusMsg)
+				}
+			})
 		}
 	})
 })(jQuery);
